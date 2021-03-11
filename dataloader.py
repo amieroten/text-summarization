381ee7925f4c8b9ab4846a759b48453a449a5017
Amie Roten <roten@ohsu.edu>
2021-03-10 20:06:34 -0800

    Pointer-generator updates.

--- dataloader.py
+++ dataloader.py
@@ -15,26 +15,40 @@ from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
 BOS_SYM = '<BOS>'
 EOS_SYM = '<EOS>'
 
-tokenizer = text.WhitespaceTokenizer()
-
 # Preprocess datasets.
-#TODO: Think about preprocessing.
 def remove_newline(article, highlights):
     return tf.strings.regex_replace(article, "\n", ""), \
            tf.strings.regex_replace(highlights, "\n", "")
 
-def add_EOS_BOS(article, highlights):
-    article = tf.strings.join([BOS_SYM, article, EOS_SYM], separator=" ")
-    highlights = tf.strings.join([BOS_SYM, highlights, EOS_SYM], separator=" ")
-    return article, highlights
+def duplicate_originals(article, highlights):
+    original_article = tf.identity(article)
+    original_highlights = tf.identity(highlights)
+    return article, highlights, original_article, original_highlights
+
+def standardize_map(article, highlights, original_article, original_highlights):
+    # Keep '!', '.', ',','?', '&', '$'.
+    punc_to_remove = '"#%\'()*+/:;<=>@[\\]^_`-{|}~'
+    regex_to_remove = f'[{re.escape(punc_to_remove)}]'
+
+    original_article = tf.strings.lower(original_article)
+    original_article = tf.strings.regex_replace(original_article, regex_to_remove, " ")
+    original_article = tf.strings.regex_replace(original_article, "([?!.,&])", r" \1 ")
+    original_article = tf.strings.join([BOS_SYM, original_article, EOS_SYM], separator=" ")
+
+    original_highlights = tf.strings.lower(original_highlights)
+    original_highlights = tf.strings.regex_replace(original_highlights, regex_to_remove, " ")
+    original_highlights = tf.strings.regex_replace(original_highlights, "([?!.,&])", r" \1 ")
+    original_highlights = tf.strings.join([BOS_SYM, original_highlights, EOS_SYM], separator=" ")
+
+    return article, highlights, original_article, original_highlights
 
 def standardize(text):
-    # Keep '!', '.', ',','?'.
-    punc_to_remove = '"#$%&\'()*+-/:;<=>@[\\]^_`{|}~'
+    punc_to_remove = '"#%\'()*+-/:;<=>@[\\]^_`-{|}~'
     regex_to_remove = f'[{re.escape(punc_to_remove)}]'
+
     text = tf.strings.lower(text)
     text = tf.strings.regex_replace(text, regex_to_remove, " ")
-    text = tf.strings.regex_replace(text, "([?.!,])", r" \1")
+    text = tf.strings.regex_replace(text, "([?!.,&])", r" \1 ")
     text = tf.strings.join([BOS_SYM, text, EOS_SYM], separator=" ")
     return text
 
@@ -79,34 +93,37 @@ def load_cnn_dailymail_deep(batch_size=1, max_vocab=5000, max_sequence=400):
         max_tokens=max_vocab,
         output_mode='int',
         output_sequence_length=max_sequence,
-        standardize=None)
+        standardize=standardize)
 
     ds_train = ds_train.map(remove_newline, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    ds_train = ds_train.map(add_EOS_BOS, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
     # "Train" vectorization layer on training articles.
     int_vectorize.adapt(ds_train.map(lambda article, highlights: article))
 
-    def int_vectorize_map(article, highlights):
+    def int_vectorize_map(article, highlights, original_article, original_highlights):
         article = tf.expand_dims(article, -1)
         highlights = tf.expand_dims(highlights, -1)
-        return int_vectorize(article), int_vectorize(highlights)
+        return int_vectorize(article), int_vectorize(highlights), original_article, original_highlights
 
+    ds_train = ds_train.map(duplicate_originals, num_parallel_calls=tf.data.experimental.AUTOTUNE)
     ds_train = ds_train.map(int_vectorize_map, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    ds_train = ds_train.map(standardize_map, num_parallel_calls=tf.data.experimental.AUTOTUNE)
     ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
     ds_train = ds_train.batch(batch_size=batch_size)
     ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
 
     ds_test = ds_test.map(remove_newline, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    ds_test = ds_test.map(add_EOS_BOS, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    ds_test = ds_test.map(duplicate_originals, num_parallel_calls=tf.data.experimental.AUTOTUNE)
     ds_test = ds_test.map(int_vectorize_map, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    ds_test = ds_test.map(standardize_map, num_parallel_calls=tf.data.experimental.AUTOTUNE)
     ds_test = ds_test.batch(batch_size=batch_size)
     ds_test = ds_test.cache()
     ds_test = ds_test.prefetch(tf.data.experimental.AUTOTUNE)
 
     ds_val = ds_val.map(remove_newline, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    ds_test = ds_test.map(add_EOS_BOS, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    ds_val = ds_val.map(duplicate_originals, num_parallel_calls=tf.data.experimental.AUTOTUNE)
     ds_val = ds_val.map(int_vectorize_map, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    ds_val = ds_val.map(standardize_map, num_parallel_calls=tf.data.experimental.AUTOTUNE)
     ds_val = ds_val.shuffle(ds_info.splits['validation'].num_examples)
     ds_val = ds_val.batch(batch_size=batch_size)
     ds_val = ds_val.prefetch(tf.data.experimental.AUTOTUNE)
@@ -133,13 +150,14 @@ def load_cnn_dailymail_experiment(batch_size=1, max_vocab=5000, max_sequence=400
     # "Train" vectorization layer on validate articles. *for debugging purposes only!!*
     int_vectorize.adapt(ds_val.map(lambda article, highlights: article))
 
-    def int_vectorize_map(article, highlights):
+    def int_vectorize_map(article, highlights, original_article, original_highlights):
         article = tf.expand_dims(article, -1)
-        print(article)
         highlights = tf.expand_dims(highlights, -1)
-        return int_vectorize(article), int_vectorize(highlights)
+        return int_vectorize(article), int_vectorize(highlights), original_article, original_highlights
 
+    ds_val = ds_val.map(duplicate_originals, num_parallel_calls=tf.data.experimental.AUTOTUNE)
     ds_val = ds_val.map(int_vectorize_map, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    ds_val = ds_val.map(standardize_map, num_parallel_calls=tf.data.experimental.AUTOTUNE)
     ds_val = ds_val.shuffle(ds_info.splits['validation'].num_examples)
     ds_val = ds_val.batch(batch_size=batch_size)
     ds_val = ds_val.prefetch(tf.data.experimental.AUTOTUNE)
